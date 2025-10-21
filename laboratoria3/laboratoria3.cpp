1de011c3bd6f09f14e870824dc0efe4caaeea572
FilipStankiewicz <04filipstankiewicz@gmail.com>
2025-10-21 16:25:37 +0200

    wszystkie zadania z 3 labów

--- laboratoria3/laboratoria3.cpp
+++ laboratoria3/laboratoria3.cpp
@@ -2,48 +2,97 @@
 //
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main() {
- /*   int a, b;                                 zad 1.
-    cout << "Podaj dwie liczby calkowite ";         
-    cin >> a >> b;
-    if (a == b) {
-        cout << "Liczby sa rowne \n";
-    }
-    else if (a > b) {
-        cout << "Wieksza liczba to: " << a << endl;
-    }
-    else {
-        cout << "Wieksza liczba to: " << b << endl;
-    }*/
-  /* float r;
-    cout << "podaj wspolczynnik r ";                      //zad 3.
-	cin >> r;
-    if (0.7 <= r && r <= 1.0) {
-        cout << "bardzo silna korelacja dodatnia\n";
-    }
-    else if (r >= 0.5 && r < 0.7) {
-        cout << "silna korelacja dodatnia\n";
-    }
-    else if (r >= 0.3 && r < 0.5) {
-        cout << "umiarkowana korelacja dodatnia\n";
-    }
-    else if (r >= 0.2 && r < 0.3) {
-        cout << "slaba korelacja dodatnia\n";
-    }
-    else if (r >= 0.0 && r < 0.2) {
-        cout << "brak korelacji\n";
-    }
-    else
-        cout << "Poza skala";*/
+    /*   int a, b;                                // zad 1.
+       cout << "Podaj dwie liczby calkowite ";
+       cin >> a >> b;
+       if (a == b) {
+           cout << "Liczby sa rowne \n";
+       }
+       else if (a > b) {
+           cout << "Wieksza liczba to: " << a << endl;
+       }
+       else {
+           cout << "Wieksza liczba to: " << b << endl;
+       }*/
+       /*  int a, b;                                 //zad 2.
+          cout << "Podaj dwie liczby calkowite ";
+          cin >> a >> b;
+          if (a == b) {
+              cout << "Liczby sa rowne \n";
+          }
+          else 
+              if (a > b) {
+              cout << "Wieksza liczba to: " << a << endl;
+          }
+          else {
+              cout << "Wieksza liczba to: " << b << endl;
+          }*/ 
+       /* float r;
+         cout << "podaj wspolczynnik r ";                      //zad 3.
+         cin >> r;
+         if (0.7 <= r && r <= 1.0) {
+             cout << "bardzo silna korelacja dodatnia\n";
+         }
+         else if (r >= 0.5 && r < 0.7) {
+             cout << "silna korelacja dodatnia\n";
+         }
+         else if (r >= 0.3 && r < 0.5) {
+             cout << "umiarkowana korelacja dodatnia\n";
+         }
+         else if (r >= 0.2 && r < 0.3) {
+             cout << "slaba korelacja dodatnia\n";
+         }
+         else if (r >= 0.0 && r < 0.2) {
+             cout << "brak korelacji\n";
+         }
+         else
+             cout << "Poza skala";*/
 
-    /*float a, b;
+             /*float a, b;
+              int wybor;
+              cout << "Podaj dwie liczby : ";          //zad 4.
+              cin >> a >> b;
+              cout << "Wybierz rodzaj dzialania :\n ";
+              cout << "1. Dodawanie\n 2. Odejmowanie\n 3. Mnozenie\n 4. Dzielenie\n";
+              cin >> wybor;
+              switch (wybor) {
+              case 1:
+                  cout << "wynik dodawania to : " << a + b << endl;
+                  break;
+              case 2:
+                  cout << "wynik odejmowania to : " << a - b << endl;
+                  break;
+              case 3:
+                  cout << "wynik mnozenia to : " << a * b << endl;
+                  break;
+              case 4:
+                  cout << "wynik dzielenia to : " << a / b << endl;
+                  break;
+              }*/
+              /*int liczba = 0;
+               while (1) {					   //zad 5.     zad. 7
+
+                       if (liczba % 2 == 0)
+                           liczba = liczba + 1;
+                       else {
+                           cout << liczba << endl;
+                           liczba = liczba + 1;
+                       }
+                   if (liczba == 200) {
+                       break;
+                   }
+               }*/
+ /*   float a, b;                                                 //zad 6.
     int wybor;
-	cout << "Podaj dwie liczby : ";          //zad 4.
-	cin >> a >> b;
+    while (1) {
+    cout << "Podaj dwie liczby : ";
+    cin >> a >> b;
     cout << "Wybierz rodzaj dzialania :\n ";
-	cout << "1. Dodawanie\n 2. Odejmowanie\n 3. Mnozenie\n 4. Dzielenie\n";
-	cin >> wybor;
+    cout << "1. Dodawanie\n 2. Odejmowanie\n 3. Mnozenie\n 4. Dzielenie\n 5. Zakoncz dzialanie\n ";
+    cin >> wybor;
     switch (wybor) {
     case 1:
         cout << "wynik dodawania to : " << a + b << endl;
@@ -57,11 +106,25 @@ int main() {
     case 4:
         cout << "wynik dzielenia to : " << a / b << endl;
         break;
-    }*/
+    case 5:
+        return 0;
+    }
+*/
+ /*  int n;
+	cout << "Podaj liczbe naturalna dodatnia : ";               //zad 8.
+	cin >> n;   
+    for (int i = 2; i < sqrt(n); i++) {
+        if (n % i == 0) {
+            cout << "nie jest to liczba pierwsza ";
+            break;
+        }
+        else
+            cout << "jest to liczba pierwsza ";
+    }*/  
 
-   
 
 
 
-    return 0;
+
+
 }
